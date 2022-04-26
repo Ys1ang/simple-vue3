6e8d6359eb41de677f39783a075e2103686fff45
solur <877377811@qq.com>
2022-04-26 16:53:50 +0800

    (fea) add scheduler stop & onStop

--- src/reactivity/__tests__/effect.spec.ts
+++ src/reactivity/__tests__/effect.spec.ts
@@ -1,15 +1,15 @@
 import {reactive} from "../reactive";
-import {effect} from "../effect";
-describe('effect',()=>{
-    it('happy path', function () {
-         const user = reactive({
-             age:10
-         });
-        let nextAge;
-        effect(()=>{
-            nextAge = user.age+1
-        });
+import {effect, stop} from "../effect";
 
+describe('effect', () => {
+    it('happy path', function () {
+        const user = reactive({
+            age: 10
+        });
+        let nextAge;
+        effect(() => {
+            nextAge = user.age + 1
+        });
         expect(nextAge).toBe(11);
         //update操作
         //测试依赖收集
@@ -17,4 +17,62 @@ describe('effect',()=>{
         expect(nextAge).toBe(12)
 
     });
+
+    it('scheduler', () => {
+        let dummy
+        let run: any
+        const scheduler = jest.fn(() => {
+            run = runner
+        })
+        const obj = reactive({foo: 1})
+        const runner = effect(
+            () => {
+                dummy = obj.foo
+            },
+            {scheduler}
+        )
+        expect(scheduler).not.toHaveBeenCalled()
+        expect(dummy).toBe(1)
+        // should be called on first trigger
+        obj.foo++
+        expect(scheduler).toHaveBeenCalledTimes(1)
+        // should not run yet
+        expect(dummy).toBe(1)
+        // manually run
+        run()
+        // should have run
+        expect(dummy).toBe(2)
+    })
+
+    it('stop', () => {
+        let dummy
+        const obj = reactive({prop: 1})
+        const runner = effect(() => {
+            dummy = obj.prop
+        })
+        obj.prop = 2
+        expect(dummy).toBe(2)
+        stop(runner)
+        obj.prop = 3
+        expect(dummy).toBe(2)
+        // stopped effect should still be manually callable
+        runner()
+        expect(dummy).toBe(3)
+    })
+
+
+    it('onStop', () => {
+        const obj = reactive({foo: 1});
+        let onStop = jest.fn();
+        let dummy;
+         const runner =effect(()=>{
+             dummy = obj.foo
+         },{onStop})
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
+    })
+
+
 })
+
+
--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,19 +1,41 @@
+import {extend} from "../shared";
+
 class ReactiveEffect{
-   private readonly _fn: any;
-    constructor(fn) {
+    private readonly _fn: any;
+     deps=[];
+     active = true;
+     onStop?:()=> void;
+    constructor(fn, public scheduler?) {
         this._fn =fn;
     }
     run(){
         activeEffect = this;
         this._fn();
     }
+    stop(){
+        if(this.active){
+            cleanupEffect(this);
+            this.active = false;
+            if(this.onStop){
+                this.onStop();
+            }
+        }
 
+    }
+
+
+}
+
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep:any)=>{
+        dep.delete(effect);
+    })
 }
 
 
 const targetMap = new Map();
 export function track(target,key){
-     // 收集effect
+    // 收集effect
     let  depsMap = targetMap.get(target);
 
     //如果不存在 depsMap ，生成一个新的map在target中注册
@@ -28,14 +50,20 @@ export function track(target,key){
         depsMap.set(key,dep)
     }
     // let  dep  = new Set();
+    if(!activeEffect) return ;
     dep.add(activeEffect);
+    activeEffect.deps.push(dep)
 }
 
 export  function trigger (target,key){
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
     for (const effect of dep){
-        effect.run()
+        if(effect.scheduler){
+            effect.scheduler()
+        }else{
+            effect.run()
+        }
     }
 }
 
@@ -43,7 +71,19 @@ export  function trigger (target,key){
 
 let activeEffect ;
 
-export function effect(fn){
-    const  _effect =  new ReactiveEffect(fn)
+export function effect(fn,options: any={}) {
+    const _effect = new ReactiveEffect(fn, options.scheduler)
+    extend(_effect,options)
     _effect.run();
+    const runner:any= _effect.run.bind(_effect);
+    runner.effect = _effect
+    return runner
 }
+
+export function stop (runner){
+    runner.effect.stop()
+}
+
+// export function  onStop(runner) {
+//     runner.effect.onStop()
+// }
