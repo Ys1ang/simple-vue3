9b98d170e56747641f23bf9cdc5a3fa812fa4b4f
solur <877377811@qq.com>
2022-05-04 23:34:36 +0800

    (fea) add isRef && unRef && proxyRefs &&单测

--- src/reactivity/__tests__/ref.spec.ts
+++ src/reactivity/__tests__/ref.spec.ts
@@ -1,5 +1,5 @@
-import {isProxy, isReadonly, readonly} from "../reactive";
-import {ref} from "../ref";
+import {isProxy, isReadonly, reactive, readonly} from "../reactive";
+import {isRef, proxyRefs, ref, unRef} from "../ref";
 import {effect} from "../effect";
 
 
@@ -36,5 +36,35 @@ describe('readonly', () => {
         expect(dummy).toBe(1);
         a.value.count = 2;
         expect(dummy).toBe(2);
+    });
+
+    it(' isRef', function () {
+        const a = reactive({count: 1});
+        const b = ref(1);
+        expect(isRef(a)).toBe(false)
+        expect(isRef(1)).toBe(false)
+        expect(isRef(b)).toBe(true)
+    });
+    it(' unRef', function () {
+        const a = ref(1);
+        expect(unRef(a)).toBe(1)
+        expect(unRef(1)).toBe(1)
+    });
+
+    it('proxyRefs', function () {
+
+        const user = {
+            age: ref(10),
+            name: 'JoJo'
+        }
+        //test get
+        const proxyUser = proxyRefs(user)
+        expect(user.age.value).toBe(10);
+        expect(proxyUser.age).toBe(10);
+        expect(proxyUser.name).toBe('JoJo');
+        //test set
+        proxyUser.age = 20;
+        expect(proxyUser.age).toBe(20);
+        expect(user.age.value).toBe(20);
     });
 })
--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -3,13 +3,14 @@ import {hasChanged, isObject} from "../shared";
 import {reactive} from "./reactive";
 
 class RefImpl {
+    public __v_isRef = true
     private dep: Set<any>;
     private _rawValue: any;
 
     constructor(value) {
         //如果是对象，用reactive处理，基础类型用ref处理
         this._rawValue = value;
-        this._value = isObject(value) ? reactive(value) : value;
+        this._value = convert(value)
         this.dep = new Set();
     }
 
@@ -24,7 +25,7 @@ class RefImpl {
         // 对比，要区分是否是reactive对象。
         if (hasChanged(newValue, this._rawValue)) {
             this._rawValue = newValue;
-            this._value = isObject(newValue) ? reactive(newValue) : newValue;
+            this._value = convert(newValue)
             triggerEffects(this.dep)
         }
     }
@@ -36,7 +37,38 @@ function trackRefValue(dep) {
     }
 }
 
+function convert(value) {
+    return isObject(value) ? reactive(value) : value;
+}
+
 
 export function ref(value) {
     return new RefImpl(value)
+}
+
+export function isRef(ref) {
+    return !!ref.__v_isRef
+}
+
+
+export function unRef(ref) {
+    return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objRefs) {
+    //ref 返回访问的value
+    //非ref 返回本身
+    return new Proxy(objRefs, {
+        get(target, key) {
+            return unRef(Reflect.get(target, key))
+        },
+        set(target, key, value) {
+            if (isRef(target[key]) && !isRef(value)) {
+                return (target[key].value = value)
+            } else {
+                return unRef(Reflect.set(target, key, value))
+            }
+
+        }
+    })
 }
